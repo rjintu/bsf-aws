097d5b0d6eef1b6845b18483d636da6c3050ef75
bcatoto <biancacatoto999@gmail.com>
2020-08-05 18:30:40 +0800

    Created form inputs and displayed results

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './fonts/Adieu-Bold.otf'
+import './fonts/WorkSans-Black.ttf'
+import './fonts/WorkSans-Regular.ttf'
+import './fonts/WorkSans-Bold.ttf'
+import './fonts/WorkSans-ExtraLight.ttf'
+import './fonts/WorkSans-Italic.ttf'
+import './fonts/WorkSans-Light.ttf'
+import './fonts/WorkSans-Medium.ttf'
+import './fonts/WorkSans-SemiBold.ttf'
+import './fonts/WorkSans-Thin.ttf'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
