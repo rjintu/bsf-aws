d967cfa45b49efd311fb093010974c72c033cedc
Rohan Jinturkar <rjintu@users.noreply.github.com>
2020-09-05 22:07:40 -0400

    add link to home page from logo
    Works for desktop and mobile

--- src/App.js
+++ src/App.js
@@ -74,15 +74,19 @@ export default class App extends React.Component {
     return (
       <div id="app">
         <Container id="header" className="margin-0" fluid>
-          <Image id="logo" className="logo" src={logo} />
           <Row>
             <Col sm={0} id="logo-container" className="text-center">
-              <Image id="logo-mobile" className="logo" src={logo} />
+              <a href="http://www.blacksheepfoods.com">
+                <Image id="logo-mobile" className="logo" src={logo} />
+              </a>
             </Col>
             <Col sm={12} id="title" className="text-center">
               Shearlock
             </Col>
           </Row>
+          <a href="http://www.blacksheepfoods.com">
+            <Image id="logo" className="logo" src={logo} />
+          </a>
         </Container>
         <Container id="description" fluid>
           Welcome to the Black Sheep Foods Shearlock tool! Shearlock uses machine learning to guide your research by generating intelligent suggestions. The suggestions are based on insights our ML model gleaned by reading over 3 million relevant food science articles. These suggestions can help guide your research by identifying key terms to research as a starting point, finding similar compounds to act as substitutes or replacements for materials you are using in an experiment, or even finding relationships between words using our analogies tools. The tool is not meant to be a search engine but rather act as a prefilter to help you choose the right starting point for a new line of inquiry to be researched using your preferred existing methods (Google, PubMed, etc.). This tool is simple, easy to use, and built to identify patterns and ideas that a human might otherwise not see. Want to get started? Just enter a search term below and click submit to begin using it.
